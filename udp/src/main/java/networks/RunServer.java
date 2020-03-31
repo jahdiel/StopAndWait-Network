51ca2532a92f08c6d2e4999a231bf94101db0430
jahdiel <jahdiel.alvarez@upr.edu>
2020-03-31 17:57:49 -0400

    Mala mia

--- udp/src/main/java/networks/RunServer.java
+++ udp/src/main/java/networks/RunServer.java
@@ -8,27 +8,5 @@ public class RunServer {
     public static void main(String[] args) {
         new Server().start();
     }
-
-    @Before
-    public void setup() throws IOException {
-        
-        client = new Client();
-    }
-
-    @Test
-    public void whenCanSendAndReceivePacket_thenCorrect1() {
-        for (int i=0; i < 10; i++) {
-            client.sendEcho("Message: " + i);
-        }
-    }
-
-    @After
-    public void tearDown() {
-        stopEchoServer();
-        client.close();
-    }
-
-    private void stopEchoServer() {
-        client.sendEcho("end");
-    }
+    
 }
